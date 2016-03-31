8bd89a448c6661e7483f1d4f6227a7551870f2a8
Garrett-S <smithgar@oregonstate.edu>
2016-03-31 13:20:27 -0700

    Fetch data from an Amazon bucket via the API

---
+++ dal/src/main/java/org/smarterbalanced/itemviewerservice/dal/AmazonApi.java
@@ -0,0 +1,50 @@
+package org.smarterbalanced.itemviewerservice.dal;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.*;
+
+public class AmazonApi {
+    private String bucketName;
+    private AmazonS3 s3connection;
+
+    public AmazonApi(String bucketName) {
+        this.bucketName = bucketName;
+        this.s3connection = new AmazonS3Client();
+    }
+    public void setBucket(String bucketName){
+        this.bucketName = bucketName;
+    }
+    public String getBucket(){
+        return this.bucketName;
+    }
+
+    public ObjectListing objectsList() {
+        ObjectListing objectListing = null;
+        try {
+            objectListing = s3connection.listObjects(new ListObjectsRequest()
+                    .withBucketName(this.bucketName));
+
+        }
+        catch (Exception e) {
+            //TODO
+        }
+        return objectListing;
+    }
+
+    public S3Object getObject (String id) {
+        S3Object object = null;
+        try {
+            object = s3connection.getObject(new GetObjectRequest(this.bucketName, id));
+        }
+        catch (Exception e) {
+            //TODO
+        }
+        return object;
+    }
+
+}
