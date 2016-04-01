e09b05479f21ea3c56bd365d6ad26100ca558c48
Garrett-S <smithgar@oregonstate.edu>
2016-04-01 15:28:30 -0700

    Added method to read file contents into memory

--- dal/src/main/java/org/smarterbalanced/itemviewerservice/dal/AmazonApi.java
+++ dal/src/main/java/org/smarterbalanced/itemviewerservice/dal/AmazonApi.java
@@ -7,6 +7,11 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.*;
+import com.amazonaws.util.IOUtils;
+import static java.lang.Math.toIntExact;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class AmazonApi {
     private String bucketName;
@@ -16,36 +21,39 @@ public class AmazonApi {
         this.bucketName = bucketName;
         this.s3connection = new AmazonS3Client();
     }
-    public void setBucket(String bucketName){
-        this.bucketName = bucketName;
-    }
-    public String getBucket(){
+
+    public String getBucketName(){
         return this.bucketName;
     }
 
-    public ObjectListing objectsList() {
-        ObjectListing objectListing = null;
-        try {
-            //TODO: request more objects if the first listObjects call does not return the full list
-            objectListing = s3connection.listObjects(new ListObjectsRequest()
-                    .withBucketName(this.bucketName));
-
-        }
-        catch (Exception e) {
-            //TODO
-        }
-        return objectListing;
-    }
-
-    public S3Object getObject (String id) {
-        S3Object object = null;
-        try {
-            object = s3connection.getObject(new GetObjectRequest(this.bucketName, id));
-        }
-        catch (Exception e) {
-            //TODO
-        }
+    public S3Object getObject (String key) {
+        S3Object object = s3connection.getObject(new GetObjectRequest(this.bucketName, key));
         return object;
     }
 
+    public byte[] getFile (S3Object object) {
+        ObjectMetadata objectMetadata = object.getObjectMetadata();
+        long awsFileSize = objectMetadata.getContentLength();
+        int fileSize;
+        try{
+            fileSize = toIntExact(awsFileSize);
+        } catch (ArithmeticException e) {
+            //in this case the file size is > INT MAX. Or greater than ~2 GB.
+            throw e;
+        }
+
+        byte[] fileData = new byte[fileSize];
+        try {
+            InputStream objectFileStream = object.getObjectContent();
+            fileData = IOUtils.toByteArray(objectFileStream);
+            objectFileStream.close();
+        } catch (IOException ex) {
+            //TODO: handle exception
+            //occurs in case of IO errors
+        } catch (NullPointerException ex) {
+            //TODO: handle exception
+            //occurs the case that the file stream fails to init.
+        }
+        return fileData;
+    }
 }
