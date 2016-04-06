d333944daf5ba20466151c99017f074423b6714a
Rutger Farry <rutgerfarry@gmail.com>
2016-04-05 18:43:49 -0700

    Revert "Feature amazon api - merging cuz"

--- core/src/main/java/org/smarterbalanced/itemviewerservice/core/App.java
+++
@@ -1,9 +0,0 @@
-package org.smarterbalanced.itemviewerservice.core;
-
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
