0eda18e1d004df363cfe73455da745ffa8d393a2
Garrett-S <smithgar@oregonstate.edu>
2016-04-06 16:11:16 -0700

    Store byte data in Redis
    Implemnted methods for RedisConnection class to allow the storage of byte
    data. Added some example usage in he main app. This should be split out
    and used to cache data from Amazon once that the Amazon API access is set
    up.

--- dal/src/main/java/org/smarterbalanced/itemviewerservice/dal/App.java
+++ dal/src/main/java/org/smarterbalanced/itemviewerservice/dal/App.java
@@ -1,43 +1,48 @@
 package org.smarterbalanced.itemviewerservice.dal;
 
 import org.smarterbalanced.itemviewerservice.dal.AmazonApi.S3UpdateChecker;
-import redis.clients.jedis.Jedis;
+import org.smarterbalanced.itemviewerservice.dal.Redis.RedisConnection;
+
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.nio.file.Files;
 
 public class App {
   public static void main(String[] args) {
     System.out.println("Hello World!");
-    (new S3UpdateChecker("url")).start();
 
-    //Code to get Redis working. Will be replaced.
+    //TODO: split this out and hook up with Amazon API instead of local filesystem.
     byte[] fileArray = null;
-    byte[] foo = "foo".getBytes(Charset.forName("UTF-8"));
+    String fileString;
+    byte[] redisResult;
+    String key = "bar";
+    byte[] byteKey = key.getBytes();
+    String redisStringResult;
+
     try {
-      fileArray = Files.readAllBytes(new File("path").toPath());
+      fileArray = Files.readAllBytes(new File("/path/file").toPath());
     } catch (IOException e) {
-      System.err.println("Failed to open file");
+      System.err.println("Failed to open file: " + e.getMessage());
+      System.exit(1);
     }
+
+    fileString = new String(fileArray);
+
+    System.out.println("Contents before storing: " + fileString);
 
     JedisPool pool = new JedisPool(new JedisPoolConfig(), "localhost");
-    Jedis jedis = null;
-    try {
-      jedis = pool.getResource();
-      jedis.set(foo, fileArray);
-      String foobar = jedis.get("foo");
-      System.out.println(foobar);
-    } finally {
-      if (jedis != null) {
-        jedis.close();
-      }
-    }
+    RedisConnection redis = new RedisConnection(pool);
+
+    redis.storeFile(byteKey, fileArray);
+
+    redisResult = redis.getFile(byteKey);
+    redisStringResult = new String(redisResult);
+
+    System.out.println("After storing: " + redisStringResult);
 
     pool.destroy();
-
   }
 }
--- dal/src/main/java/org/smarterbalanced/itemviewerservice/dal/Redis/RedisConnection.java
+++ dal/src/main/java/org/smarterbalanced/itemviewerservice/dal/Redis/RedisConnection.java
@@ -1,6 +1,9 @@
 package org.smarterbalanced.itemviewerservice.dal.Redis;
 
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+
+import java.nio.charset.Charset;
 
 public class RedisConnection {
   private JedisPool pool;
@@ -9,16 +12,56 @@ public class RedisConnection {
     this.pool = pool;
   }
 
-  public void setFile(String key) {
-
+  public void storeFile(byte[] key, byte[] fileData) {
+    Jedis jedis = null;
+    String result = null;
+    try {
+      jedis = this.pool.getResource();
+      result = jedis.set(key, fileData);
+    } catch (Exception e) {
+      System.err.println("ERROR: Failed to get a Redis connection while setting.");
+    } finally {
+      if (jedis != null) {
+        jedis.close();
+      }
+    }
+    if (!result.equals("OK")) {
+      System.err.println("ERROR: Failed to store file. Reason " + result);
+    }
   }
 
-  public byte[] getFile(String key) {
+  public byte[] getFile(byte[] key) {
     byte[] fileContents = null;
+    Jedis jedis = null;
+    String result = null;
+    try {
+      jedis = this.pool.getResource();
+      fileContents = jedis.get(key);
+    } catch (Exception e) {
+      System.err.println("ERROR: Failed to get a Redis connection while getting.");
+    } finally {
+      if (jedis != null) {
+        jedis.close();
+      }
+    }
     return fileContents;
   }
 
   public void removeFile(String key) {
-
+    Jedis jedis = null;
+    Long result = 0L;
+    try {
+      jedis = this.pool.getResource();
+      result = jedis.del(key);
+    } catch (Exception e) {
+      System.err.println("ERROR: Failed to get a Redis connection while deleting.");
+    } finally {
+      if (jedis != null) {
+        jedis.close();
+      }
+    }
+    if (result != 1L) {
+      System.err.println("ERROR: Failed to get delete object.");
+    }
   }
 }
