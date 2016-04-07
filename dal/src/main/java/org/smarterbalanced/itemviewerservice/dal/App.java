635ae53e36b3d4d44aa0c25b081a474e34b1feff
Rutger Farry <rutgerfarry@gmail.com>
2016-04-07 13:00:53 -0700

    Various things to comply to the style guide

--- dal/src/main/java/org/smarterbalanced/itemviewerservice/dal/App.java
+++ dal/src/main/java/org/smarterbalanced/itemviewerservice/dal/App.java
@@ -2,11 +2,11 @@ package org.smarterbalanced.itemviewerservice.dal;
 
 import org.smarterbalanced.itemviewerservice.dal.AmazonApi.S3UpdateChecker;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        (new S3UpdateChecker("url")).start();
-    }
+public class App {
+
+  public static void main( String[] args ) {
+    System.out.println( "Hello World!" );
+    (new S3UpdateChecker("url")).start();
+  }
+
 }
