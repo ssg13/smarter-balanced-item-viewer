78743553fa34f7ed3c230e4f42c93d3e5455f176
Garrett Smith <garrett.smith@oregonstate.edu>
2016-04-28 12:35:19 -0700

    Fixed formatting to comply with style guide.

--- dal/src/main/java/org/smarterbalanced/itemviewerservice/dal/App.java
+++ dal/src/main/java/org/smarterbalanced/itemviewerservice/dal/App.java
@@ -9,8 +9,6 @@ import redis.clients.jedis.JedisPool;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -29,8 +27,8 @@ public class App {
    *
    * @param args There are no input arguments needed.
    */
-  public static void main( String[] args ) {
-    System.out.println( "Starting redis storage example." );
+  public static void main(String[] args) {
+    System.out.println("Starting redis storage example.");
 
     String fileName = "";
     String packageBucket = "";
@@ -41,7 +39,8 @@ public class App {
     long delta;
     long startTime;
     long endTime;
-    String path = System.getProperty("user.home") + "/sb-redis-example/";;
+    String path = System.getProperty("user.home") + "/sb-redis-example/";
+    ;
     Path workingDirectory = Paths.get(path);
     byte[] zip;
     byte[] fileData;
@@ -68,31 +67,30 @@ public class App {
       System.exit(1);
     }
 
-
-      for (String key : packageKeys) {
-        try {
-          System.out.println("Starting zip file download from Amazon S3 bucket.");
-          startTime = new Date().getTime();
-          zip = amazonApi.getS3File(key);
-          endTime = new Date().getTime();
-          delta = endTime - startTime;
-          System.out.println("Finished downloading zip file from Amazon S3.");
-          System.out.println("Downloading the zip from Amazon took " + delta + " milliseconds.");
-          System.out.println("Starting to write zip file to disk.");
-          startTime = new Date().getTime();
-          FileOutputStream fos = new FileOutputStream(path + key);
-          fos.write(zip);
-          fos.close();
-          endTime = new Date().getTime();
-          delta = endTime - startTime;
-          System.out.println("Finished writing zip file to disk.");
-          System.out.println("Writing zip file to disk took " + delta + " milliseconds.");
-        } catch (Exception e) {
-          System.out.println("Error fetching files from S3.");
-          System.out.println(e.getMessage());
-          System.exit(1);
-        }
+    for (String key : packageKeys) {
+      try {
+        System.out.println("Starting zip file download from Amazon S3 bucket.");
+        startTime = new Date().getTime();
+        zip = amazonApi.getS3File(key);
+        endTime = new Date().getTime();
+        delta = endTime - startTime;
+        System.out.println("Finished downloading zip file from Amazon S3.");
+        System.out.println("Downloading the zip from Amazon took " + delta + " milliseconds.");
+        System.out.println("Starting to write zip file to disk.");
+        startTime = new Date().getTime();
+        FileOutputStream fos = new FileOutputStream(path + key);
+        fos.write(zip);
+        fos.close();
+        endTime = new Date().getTime();
+        delta = endTime - startTime;
+        System.out.println("Finished writing zip file to disk.");
+        System.out.println("Writing zip file to disk took " + delta + " milliseconds.");
+      } catch (Exception e) {
+        System.out.println("Error fetching files from S3.");
+        System.out.println(e.getMessage());
+        System.exit(1);
       }
+    }
 
 
     for (String key : packageKeys) {
@@ -134,6 +132,7 @@ public class App {
     System.out.println("Writing files to disk took " + delta + " milliseconds.");
 
   }
+
   private static String getConfigS3Bucket() throws IOException {
     String configLocation = "dal-config.properties";
     String bucket;
