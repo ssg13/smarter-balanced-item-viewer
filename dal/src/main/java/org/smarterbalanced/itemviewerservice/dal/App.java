0e6404673d0817868f818eaf48b9bab9756a30e8
Garrett Smith <garrett.smith@oregonstate.edu>
2016-04-28 12:28:06 -0700

    Moved config file to dal layer
    Moved the config file to the dal layer and added a method to main to
    read off the Amazon S3 bucket.
    Adjusted main code to use the config file to determine the packages in
    Amazon.

--- dal/src/main/java/org/smarterbalanced/itemviewerservice/dal/App.java
+++ dal/src/main/java/org/smarterbalanced/itemviewerservice/dal/App.java
@@ -15,6 +15,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Date;
+import java.util.List;
 import java.util.Properties;
 import java.util.Set;
 
@@ -31,31 +32,22 @@ public class App {
   public static void main( String[] args ) {
     System.out.println( "Starting redis storage example." );
 
-    Properties config = new Properties();
-    InputStream configInput;
-
     String fileName = "";
     String packageBucket = "";
+    List<String> packageKeys = null;
     AmazonFileApi amazonApi;
     JedisPool pool = new JedisPool();
     RedisConnection redis = new RedisConnection(pool);
     long delta;
     long startTime;
     long endTime;
-    System.getProperty("user.home");
-    String path = System.getProperty("user.home") + "/sb-redis-example/";
-    Path filePath = Paths.get(path);
+    String path = System.getProperty("user.home") + "/sb-redis-example/";;
+    Path workingDirectory = Paths.get(path);
     byte[] zip;
     byte[] fileData;
 
     try {
-      configInput = new FileInputStream("dal-config.properties");
-      config.load(configInput);
-
-      fileName = config.getProperty("packageName");
-      packageBucket = config.getProperty("S3bucket");
-
-      configInput.close();
+      packageBucket = getConfigS3Bucket();
     } catch (IOException e) {
       System.err.println("Failed to load configuration file.");
       System.exit(1);
@@ -63,48 +55,61 @@ public class App {
     amazonApi = new AmazonFileApi(packageBucket);
 
     try {
-      Files.createDirectories(filePath);
+      packageKeys = amazonApi.getAllKeys();
+      fileName = packageKeys.get(0);
+    } catch (Exception e) {
+      System.exit(1);
+    }
+
+    try {
+      Files.createDirectories(workingDirectory);
     } catch (IOException e) {
       System.err.println("Failed to create destination directory for zip file download.");
       System.exit(1);
     }
-    try {
-      System.out.println("Starting zip file download from Amazon S3 bucket.");
-      startTime = new Date().getTime();
-      zip = amazonApi.getS3File(fileName);
-      endTime = new Date().getTime();
-      delta = endTime - startTime;
-      System.out.println("Finished downloading zip file from Amazon S3.");
-      System.out.println("Downloading the zip from Amazon took " + delta + " milliseconds.");
-      System.out.println("Starting to write zip file to disk.");
-      startTime = new Date().getTime();
-      FileOutputStream fos = new FileOutputStream(path + fileName);
-      fos.write(zip);
-      fos.close();
-      endTime = new Date().getTime();
-      delta = endTime - startTime;
-      System.out.println("Finished writing zip file to disk.");
-      System.out.println("Writing zip file to disk took " + delta + " milliseconds.");
-    } catch (Exception e) {
-      System.out.println("Error fetching files from S3.");
-      System.out.println(e.getMessage());
-      System.exit(1);
-    }
 
-    try {
-      System.out.println("Started unzipping package contents to Redis.");
-      startTime = new Date().getTime();
-      StoreZip.unpackToRedis(path + fileName, redis);
-      endTime = new Date().getTime();
-      delta = endTime - startTime;
-      System.out.println("Finished unzipping package contents to Redis.");
-      System.out.println("Unpacking the zip file to Redis took " + delta + " milliseconds.");
-    } catch (Exception e) {
-      System.err.println("Failed to store package contents in Redis");
-      System.err.println(e.getMessage());
-      System.exit(1);
-    }
 
+      for (String key : packageKeys) {
+        try {
+          System.out.println("Starting zip file download from Amazon S3 bucket.");
+          startTime = new Date().getTime();
+          zip = amazonApi.getS3File(key);
+          endTime = new Date().getTime();
+          delta = endTime - startTime;
+          System.out.println("Finished downloading zip file from Amazon S3.");
+          System.out.println("Downloading the zip from Amazon took " + delta + " milliseconds.");
+          System.out.println("Starting to write zip file to disk.");
+          startTime = new Date().getTime();
+          FileOutputStream fos = new FileOutputStream(path + key);
+          fos.write(zip);
+          fos.close();
+          endTime = new Date().getTime();
+          delta = endTime - startTime;
+          System.out.println("Finished writing zip file to disk.");
+          System.out.println("Writing zip file to disk took " + delta + " milliseconds.");
+        } catch (Exception e) {
+          System.out.println("Error fetching files from S3.");
+          System.out.println(e.getMessage());
+          System.exit(1);
+        }
+      }
+
+
+    for (String key : packageKeys) {
+      try {
+        System.out.println("Started unzipping package contents to Redis.");
+        startTime = new Date().getTime();
+        StoreZip.unpackToRedis(path + key, redis);
+        endTime = new Date().getTime();
+        delta = endTime - startTime;
+        System.out.println("Finished unzipping package contents to Redis.");
+        System.out.println("Unpacking the zip file to Redis took " + delta + " milliseconds.");
+      } catch (Exception e) {
+        System.err.println("Failed to store package contents in Redis");
+        System.err.println(e.getMessage());
+        System.exit(1);
+      }
+    }
 
 
     Set<String> keys = redis.listKeys();
@@ -113,9 +118,9 @@ public class App {
     try {
       for (String key : keys) {
         fileData = redis.getByteFile(key);
-        filePath = Paths.get(path + key);
-        Files.createDirectories(filePath.getParent());
-        Files.write(filePath, fileData);
+        workingDirectory = Paths.get(path + key);
+        Files.createDirectories(workingDirectory.getParent());
+        Files.write(workingDirectory, fileData);
       }
     } catch (Exception e) {
       System.err.println("Failed to extract file from Redis to disk.");
@@ -128,22 +133,17 @@ public class App {
     System.out.println("Finished writing files from Redis to disk.");
     System.out.println("Writing files to disk took " + delta + " milliseconds.");
 
-    /*
-    try {
-      zip = amazonApi.getS3File("IrpContentPackage.zip");
-      InputStream zipStream = new ByteArrayInputStream(zip);
-      startTime = new Date().getTime();
-      StoreZip.unpackToBucket(zipStream, "test-contentpack");
-      endTime = new Date().getTime();
-      delta = endTime - startTime;
-      System.out.println("Storing the zip contents to an Amazon bucket took "
-              + delta + " milliseconds.");
-    } catch (Exception e) {
-      System.out.println("Error storing files in S3.");
-      System.out.println(e.getMessage());
-      System.exit(1);
-    }
-    */
+  }
+  private static String getConfigS3Bucket() throws IOException {
+    String configLocation = "dal-config.properties";
+    String bucket;
+    Properties dalProperties = new Properties();
+    FileInputStream configInput;
+    configInput = new FileInputStream(configLocation);
+    dalProperties.load(configInput);
+    bucket = dalProperties.getProperty("S3bucket");
+    configInput.close();
+    return bucket;
   }
 
 }
