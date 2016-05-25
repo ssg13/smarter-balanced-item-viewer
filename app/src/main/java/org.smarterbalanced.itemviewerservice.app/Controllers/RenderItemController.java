cf2ce16221c2cac84c1bda80861223346da3ded6
Garrett Smith <garrett.smith@oregonstate.edu>
2016-05-25 16:15:07 -0700

    Added tests for RenderItemController
    Check validate that the parameters are being parsed out of the url

--- app/src/main/java/org.smarterbalanced.itemviewerservice.app/Controllers/RenderItemController.java
+++ app/src/main/java/org.smarterbalanced.itemviewerservice.app/Controllers/RenderItemController.java
@@ -1,26 +1,43 @@
 package org.smarterbalanced.itemviewerservice.app.Controllers;
 
-import org.smarterbalanced.itemviewerservice.core.Models.ItemModel;
-import org.springframework.web.bind.annotation.*;
+import org.smarterbalanced.itemviewerservice.core.Models.ItemRequestModel;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * REST API controller for rendering items.
+ */
 @RestController
+@EnableAutoConfiguration
 public class RenderItemController {
 
-  @RequestMapping(value="/item/{itemID}", method= RequestMethod.GET)
-  public ItemModel getContent(@PathVariable("itemID") String id,
-                              @RequestParam(value = "isaap", required = false) final String codes) {
+  /**
+   * Returns content.
+   *
+   * @param id    Item and bank ID separated by a "-"
+   * @param codes Feature codes delimited by semicolons.
+   * @return content object.
+   */
+  @RequestMapping(value = "/item/{itemID}", method = RequestMethod.GET)
+  public ItemRequestModel getContent(@PathVariable("itemID") String id,
+                                     @RequestParam(value = "isaap", required = false)
+                                     final String codes) {
     String[] parsedIds = id.split("-");
     String bank = parsedIds[0];
     String itemId = parsedIds[1];
     String[] codesArray = {};
-    if (codes != null) {
+    if ( codes != null ) {
       codesArray = codes.split(";");
     }
-    ItemModel request = new ItemModel(itemId, bank, codesArray);
+    ItemRequestModel request = new ItemRequestModel(itemId, bank, codesArray);
     System.out.println("Bank ID: " + request.getBank());
     System.out.println("Item ID: " + request.getId());
     System.out.println("Codes:");
-    for(String code : request.getFeatureCodes()) {
+    for (String code : request.getFeatureCodes()) {
       System.out.println(code);
     }
     return request;
